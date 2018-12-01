32b0315d8ebfc622fa50126e341cdfab23e37e25
TOXICDARKNESS <danielantonio2003@gmail.com>
2018-12-01 15:00:46 +0000

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,4 @@
+echo off
+:start
+RustDedicated.exe -batchmode +server.port 28015 +server.level "Procedural Map" +server.seed 1234 +server.worldsize 350 +server.maxplayers 100  +server.hostname "[EU]iLyfe.co.uk | Roleplay" +server.description "Welcome to ilyfe! We are an arma 3 / rust roleplay community. Join our discord here: https://discord.gg/HgaWHA4   Join our TS here: ts.ilyfe.co.uk" +server.url "http://malden-gov.uk" +server.headerimage "" +server.identity "[EU]iLyfe.co.uk | Roleplay" +rcon.port 28016 +rcon.password fumopa +rcon.web 1
+goto start
